381161233ad000b040b3814bd8423f5c308ed3a4
Pavel <39xanax@gmail.com>
2026-08-08 14:19:30 +0500

    chore: version 1.1.0

--- apps/NetglassMac/Sources/NetglassMac/Operation/OperationModels.swift
+++ apps/NetglassMac/Sources/NetglassMac/Operation/OperationModels.swift
@@ -258,7 +258,7 @@ public struct OperationBundle: Codable, Equatable, Sendable {
                 events: [OperationEvent], snapshotIn: OperationSnapshot,
                 snapshotOut: OperationSnapshot?, cleanupReport: CleanupReport?) {
         self.app = "Netglass"
-        self.version = "1.0.0"
+        self.version = "1.1.0"
         self.operation = operation
         self.warnings = warnings
         self.events = events
